d05895547f9b68ccddd8da1c02d300aa04de5005
ronnieetoljr-rgb <ronnieetoljr@gmail.com>
2026-01-20 02:27:03 +0800

    code

---
+++ DEC13_WEEK11/PART1_DETECTOR.py
@@ -0,0 +1,8 @@
+
+#for root, dirs, files in os.walk(os.getcwd()):
+    #for file in files:
+        #if file.endswith("CABRADILLA_JOHN_LENARD_UTS_ESSAY2_DEC13.docx"):
+        #if file.endswith:
+            #print(file)
+
+#for root, dirs, files in os.walk(BASE_FILE):
