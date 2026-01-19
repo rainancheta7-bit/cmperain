eb0dd7569ce1c2c0981d4f9949417648f8734fe2
ronnieetoljr-rgb <ronnieetoljr@gmail.com>
2026-01-20 02:32:01 +0800

    code

---
+++ NOV8_WEEK7/PART1.py
@@ -0,0 +1,29 @@
+#demo 16
+#if and if else and else
+#example 1
+
+myGrade = 75
+
+if myGrade >= 97:
+    print("Grade is: 1.00")#statement #body
+elif myGrade >= 97:
+    print("Grade is: 1.25")
+elif myGrade >= 94:
+    print("Grade is: 1.50")
+elif myGrade >= 91:
+    print("Grade is: 1.75")
+elif myGrade >= 88:
+    print("Grade is: 2.00")
+elif myGrade >= 85:
+    print("Grade is: 2.25")
+elif myGrade >= 82:
+    print("Grade is: 2.50")
+elif myGrade >= 79:
+    print("Grade is: 2.75")
+elif myGrade >= 76:
+    print("Grade is: 3.00")
+
+else:
+    print("5.0/D/W/INC")
+
+print("After If Else Condition")
